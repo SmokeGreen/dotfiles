21735a68815cb6af5cdb1ccfac170d8706f53214
dev01 <dev01@example.com>
2018-01-07 10:19:05 +0000

    First commit

---
+++ .bash_profile
@@ -0,0 +1,7 @@
+export PS1="\u$ "
+
+# read .bashrc
+if [ -f ~/.bashrc ] ; then
+. ~/.bashrc
+fi
+
---
+++ .emacs.d/init.el
@@ -0,0 +1,18 @@
+;;test
+;;(define-key global-map (kbd "C-t") 'other-window)
+
+;;line-number-mode
+(line-number-mode t)
+
+;;linum-mode
+(global-linum-mode t)
+
+;;tab
+(setq-default tab-width 4)
+
+;;font
+(set-face-attribute 'default nil
+					:family "Menlo"
+					:height 120)
+
+
