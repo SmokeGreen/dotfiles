964c50c812b62b72ba179b60ed77e79819c82640
developer01 <developer01@example.com>
2018-12-11 01:43:39 +0000

    commit at 20181211

--- init.sh
+++ init.sh
@@ -4,5 +4,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.sqliterc ~/.sqliterc
 
 echo "dotfiles are linked."
